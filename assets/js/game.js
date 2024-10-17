17f73cc8a70e034a5315223a0d7820c394b37dd3
Adnan Hasnain <devadnan27@gmail.com>
2024-10-17 16:16:28 +0500

    Add Background Scroll

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,23 +4,47 @@ import { Platform } from './platform.js';
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-// console.log(ctx);
 
 canvas.width = 1024;
 canvas.height = 576;
 
 const player = new Player();
-const platform = new Platform(0, canvas.height - 150, canvas.width - 100, 30);
-
+let platformScrollSpeed = 0;
+let gameOver = false;
+const platforms = [
+    new Platform(0, canvas.height - 150, 300, 30),
+    new Platform(400, canvas.height - 250, 200, 30),
+    new Platform(700, canvas.height - 350, 300, 30),
+    new Platform(200, canvas.height - 450, 250, 30),
+];
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    player.update(ctx);
-    platform.draw(ctx);
-    checkCollision(player, platform);
+    
+    if (!gameOver) {
+        platforms.forEach(platform => {
+            platform.position.x -= platformScrollSpeed;
+            if (platform.position.x + platform.size.width < 0) {
+                platform.position.x = canvas.width;
+            } else if (platform.position.x > canvas.width) {
+                platform.position.x = -platform.size.width;
+            }
+        });
+        
+        player.update(ctx);
+        platforms.forEach(platform => {
+            platform.draw(ctx);
+            checkCollision(player, platform);
+        });
+        
+        checkGameOver();
+    }
+    player.draw(ctx);
+    
     requestAnimationFrame(animate);
 }
 
 animate();
+
 
 function checkCollision(player, platform) {
     const playerBottom = player.position.y + player.size.height;
@@ -60,36 +84,57 @@ function checkCollision(player, platform) {
 
 
 
+function checkGameOver() {
+    if (player.position.y + player.size.height > canvas.height) {
+        gameOver = true;
+        console.log("Game Over");
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        platformScrollSpeed = 0;
+    }
+}
+
+
+
+
+
 window.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'ArrowRight':
-            player.velocity.x = 5;
-            break;
-        case 'ArrowLeft':
-            player.velocity.x -= 5;
-            break;
-        case 'ArrowUp':
-            player.velocity.y -= 5;
-            break;
-        case 'ArrowDown':
-            player.velocity.y += 5;
-            break;
+    if (!gameOver) {
+        switch (e.key) {
+            case 'ArrowRight':
+            case 'd':
+            case 'D':
+                platformScrollSpeed = 3; 
+                player.velocity.x = 5; 
+                break;
+            case 'ArrowLeft':
+            case 'a':
+            case 'A':
+                platformScrollSpeed = -3; 
+                player.velocity.x = -5; 
+                break;
+            case 'ArrowUp':
+            case 'w':
+            case 'W':
+            case ' ':
+                player.velocity.y = -10; 
+                break;
+        }
     }
 });
 
 window.addEventListener('keyup', (e) => {
-    switch (e.key) {
-        case 'ArrowRight':
-            player.velocity.x = 0;
-            break;
-        case 'ArrowLeft':
-            player.velocity.x = 0;
-            break;
-        case 'ArrowUp':
-            player.velocity.y = 0;
-            break;
-        case 'ArrowDown':
-            player.velocity.y = 0;
-            break;
+    if (!gameOver) {
+        switch (e.key) {
+            case 'ArrowRight':
+            case 'd':
+            case 'D':
+            case 'ArrowLeft':
+            case 'a':
+            case 'A':
+                platformScrollSpeed = 0;
+                player.velocity.x = 0;
+                break;
+        }
     }
 });
--- assets/js/player.js
+++ assets/js/player.js
@@ -1,7 +1,7 @@
 export class Player {
     constructor() {
         this.position = {
-            x: 0,
+            x: 100,
             y: 0,
         };
         this.size = {
@@ -13,41 +13,30 @@ export class Player {
             x: 0,
             y: 0,
         };
-        this.draw = function (ctx) {
-            ctx.fillStyle = 'red';
-            ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+    }
+
+    update(ctx) {
+        this.velocity.y += this.gravity;
+        this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
+
+        // Only check top and side boundaries
+        if (this.position.y < 0) {
+            this.position.y = 0;
+            this.velocity.y = 0;
         }
-
-        this.update = function (ctx) {
-            this.velocity.y += this.gravity;
-
-            // Check vertical boundaries
-            if (this.position.y + this.velocity.y > 0 &&
-                this.position.y + this.size.height + this.velocity.y < ctx.canvas.height) {
-                this.position.y += this.velocity.y;
-            } else {
-                this.velocity.y = 0;
-                if (this.position.y <= 0) {
-                    this.position.y = 0;
-                }
-                if (this.position.y + this.size.height >= ctx.canvas.height) {
-                    this.position.y = ctx.canvas.height - this.size.height;
-                }
-            }
-            // Check horizontal boundaries
-            if (this.position.x + this.velocity.x > 0 && 
-                this.position.x + this.size.width + this.velocity.x < ctx.canvas.width) {
-                this.position.x += this.velocity.x;
-            } else {
-                this.velocity.x = 0;
-                if (this.position.x <= 0) {
-                    this.position.x = 0;
-                }
-                if (this.position.x + this.size.width >= ctx.canvas.width) {
-                    this.position.x = ctx.canvas.width - this.size.width;
-                }
-            }
-            this.draw(ctx);
+        if (this.position.x < 0) {
+            this.position.x = 0;
+            this.velocity.x = 0;
+        }
+        if (this.position.x + this.size.width > ctx.canvas.width) {
+            this.position.x = ctx.canvas.width - this.size.width;
+            this.velocity.x = 0;
         }
     }
 }
